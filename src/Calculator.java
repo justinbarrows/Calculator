45f2a0c126c0f07ce0b5a727c3b451660af00397
jsb <jbarrows1030@gmail.com>
2022-09-03 20:31:00 -0400

    feat: numbers on click show in text box

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Calculator {
     private JPanel Calculator;
@@ -23,6 +25,81 @@ public class Calculator {
     private JButton a6Button;
     private JButton a3Button;
     private JButton button20;
+
+    public Calculator() {
+        ACButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtDisplay.setText("");
+            }
+        });
+        a7Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtDisplay.setText(txtDisplay.getText() + a7Button.getText());
+            }
+        });
+        a8Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtDisplay.setText(txtDisplay.getText() + a8Button.getText());
+            }
+        });
+        a9Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtDisplay.setText(txtDisplay.getText() + a9Button.getText());
+            }
+        });
+        a4Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtDisplay.setText(txtDisplay.getText() + a4Button.getText());
+            }
+        });
+        a5Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtDisplay.setText(txtDisplay.getText() + a5Button.getText());
+            }
+        });
+        a6Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtDisplay.setText(txtDisplay.getText() + a6Button.getText());
+            }
+        });
+        a1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtDisplay.setText(txtDisplay.getText() + a1Button.getText());
+            }
+        });
+        a2Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtDisplay.setText(txtDisplay.getText() + a2Button.getText());
+            }
+        });
+        a3Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtDisplay.setText(txtDisplay.getText() + a3Button.getText());
+            }
+        });
+        a0Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtDisplay.setText(txtDisplay.getText() + a0Button.getText());
+            }
+        });
+        a00Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtDisplay.setText(txtDisplay.getText() + a00Button.getText());
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Calculator");
